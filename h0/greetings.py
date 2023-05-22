6d533bf2eae9a03c7e03ec02592e850e16c1f397
Olli Myyryläinen <myyrylainen.olli@gmail.com>
2023-05-22 13:58:05 +0300

    Lisätty tehtävä h0

---
+++ h0/greetings.py
@@ -0,0 +1,8 @@
+def sayGreetingsTo(name):
+    print("Hello, my friend " + name + "! I am so glad that you could join me for another adventure.")
+
+def main():
+    name = input("Who are you? > ")
+    sayGreetingsTo(name)
+
+main()
